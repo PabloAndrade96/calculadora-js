e0d6d45336d96799af148caf6ea29616806606b7
PabloAndrade96 <pablopopandrade@outlook.com>
2023-05-18 18:52:18 -0300

    darkmode agregado

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,30 @@ const pre_value = document.querySelector("#pre-value");
 const cur_value = document.querySelector("#cur-value");
 const buttons = document.querySelectorAll(".calculator__button");
 
+window.addEventListener("load", () => {
+  loadDarkmode();
+});
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", ({ target }) => {
+    const btn = target.id;
+
+    if (btn === "=") {
+      calculate();
+    } else if (btn === "delete") {
+      de(btn);
+    } else if (btn === "clear") {
+      clear(btn);
+    } else if (btn === "darkmode") {
+      darkmode(btn);
+    } else {
+      showDisplay(btn);
+    }
+  });
+});
+
+/* Functions */
+
 const showDisplay = (btn) => {
   if (btn === "*") {
     cur_value.innerHTML += `&#10005;`;
@@ -41,18 +65,29 @@ const calculate = () => {
   cur_value.textContent = eval(new_value);
 };
 
-buttons.forEach((btn) => {
-  btn.addEventListener("click", ({ target }) => {
-    const btn = target.id;
+const darkmode = (btn) => {
+  const body = document.querySelector("body");
+  const icon = document.querySelector(`#${btn} i`);
 
-    if (btn === "=") {
-      calculate();
-    } else if (btn === "delete") {
-      de(btn);
-    } else if (btn === "clear") {
-      clear(btn);
-    } else {
-      showDisplay(btn);
-    }
-  });
-});
+  body.classList.toggle("darkmode");
+
+  if (icon.classList.contains("bi-moon")) {
+    icon.classList.replace("bi-moon", "bi-sun");
+  } else {
+    icon.classList.replace("bi-sun", "bi-moon");
+  }
+
+  storeDarkmode(body.classList.contains("darkmode"));
+};
+
+const loadDarkmode = () => {
+  const darkmode = localStorage.getItem("darkmode");
+
+  if (!darkmode) {
+    storeDarkmode("false");
+  } else if (darkmode === "true") {
+    document.body.classList.add("darkmode");
+  }
+};
+
+const storeDarkmode = (value) => localStorage.setItem("darkmode", value);
